4de5dcf4eb9ecc5e8b8669877b4fa5c19bd4ccc7
ceydaecearslan <ceydaecearslan@gmail.com>
2025-04-25 03:53:51 +0300

    ability hud widgets created
    only for visuals, code not implemented

---
+++ Source/TopDown/Public/UI/HUD/MainAbilities.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainAbilities.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API UMainAbilities : public UUserWidget
+{
+	GENERATED_BODY()
+};
